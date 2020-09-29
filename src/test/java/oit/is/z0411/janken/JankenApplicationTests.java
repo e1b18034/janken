9e232694d7e28e1a1b0dc921c31c82de0af5dc65
e1b18034 <e1b18034@oit.ac.jp>
2020-09-29 16:21:33 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/z0411/janken/JankenApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0411.janken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
